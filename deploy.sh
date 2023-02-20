cddf5c7740947668591d14be10633192fdd33717
Majid <majid.ypd@gmail.com>
2023-02-20 16:52:39 +0100

    fix 11

--- deploy.sh
+++ deploy.sh
@@ -10,11 +10,13 @@ exitMessage() {
 tag=${GITHUB_TAG-}
 echo "recived:"
 echo $tag
-if [[ $tag =~ ^v[0-9]{1}\.[0-9]{1,2}\.[0-9]{1,3}$ ]]; then
-   echo "Deploying ${tag} into production account in ${AWS_REGION_PROD}..."
-else
-   exitMessage 0 "not a valid tag, not deploying"
-fi
+# if [[ $tag =~ ^v[0-9]{1}\.[0-9]{1,2}\.[0-9]{1,3}$ ]]; then
+#    echo "Deploying ${tag} into production account in ${AWS_REGION_PROD}..."
+# else
+#    exitMessage 0 "not a valid tag, not deploying"
+# fi
 
-echo "after xxxxx"
-echo $tag
+# echo "after xxxxx"
+# echo $tag
+
+go run main.go
--- main.go
+++ main.go
@@ -4,12 +4,16 @@ import
 (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main()  {
-	fmt.Println("server is running ...")
-	http.HandleFunc("/",X)
-	http.ListenAndServe(":8080",nil)
+	// fmt.Println("server is running ...")
+	// http.HandleFunc("/",X)
+	// http.ListenAndServe(":8080",nil)
+
+	d := os.Getenv("CHAT_TAG")
+	fmt.Println("RECIVED in ENV:", d)
 }
 
 func X(w http.ResponseWriter, r *http.Request){
