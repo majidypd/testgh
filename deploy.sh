513078260bde015c8996880d54d8333c624f4cbe
Majid <majid.ypd@gmail.com>
2023-02-20 15:01:23 +0100

    fix 7

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
+exitMessage() {
+   echo "${2-exiting}"
+   exit "${1}"
+}
+
+
+
 tag=${GITHUB_TAG-}
 if [[ $tag =~ ^v[0-9]{1}\.[0-9]{1,2}\.[0-9]{1,3}$ ]]; then
    echo "Deploying ${tag} into production account in ${AWS_REGION_PROD}..."
