1229b6df056752b5f94d53f2d312b625560458ce
Majid <majid.ypd@gmail.com>
2023-02-20 15:00:22 +0100

    fix 6

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
-tag=${GITHUB_TAG}
+tag=${GITHUB_TAG-}
+if [[ $tag =~ ^v[0-9]{1}\.[0-9]{1,2}\.[0-9]{1,3}$ ]]; then
+   echo "Deploying ${tag} into production account in ${AWS_REGION_PROD}..."
+else
+   exitMessage 0 "not a valid tag, not deploying"
+fi
 
 echo $tag
