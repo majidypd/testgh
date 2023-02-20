ddebb127a9a95277432024b11f04283252fa4689
Majid <majid.ypd@gmail.com>
2023-02-20 16:46:17 +0100

    fix 9

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,8 @@ exitMessage() {
 
 
 tag=${GITHUB_TAG-}
+echo "recived:"
+echo $tag
 if [[ $tag =~ ^v[0-9]{1}\.[0-9]{1,2}\.[0-9]{1,3}$ ]]; then
    echo "Deploying ${tag} into production account in ${AWS_REGION_PROD}..."
 else
